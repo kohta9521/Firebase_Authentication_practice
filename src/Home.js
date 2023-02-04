18bdf88b8c29fae98804ce888ccfb931f07c808c
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-05 06:45:24 +0900

    add SignINWithGoogle Button function

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,22 @@ import React from 'react'
 function Home() {
     return (
         <div>
-            
+            <SignInButton />
         </div>
     )
 }
 
 export default Home
+
+//Googleボタンでサインイン
+function SignInButton() {
+    const signInWithGoogle = () => {
+        // firebaseを使用してGoogleでログインする
+    };
+
+    return (
+        <button onClick={signInWithGoogle}>
+            <p>Googleでサインイン</p>
+        </button>
+    )
+}
