e57b9c5668269aec5a9e644889ec0b9578329ac0
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-05 06:50:06 +0900

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAoca3b576LdEIYL8_kZzzMUA58DZlbnzo",
+    authDomain: "fir-login-81c44.firebaseapp.com",
+    projectId: "fir-login-81c44",
+    storageBucket: "fir-login-81c44.appspot.com",
+    messagingSenderId: "677358945171",
+    appId: "1:677358945171:web:5833b3afb5989af5025e66"
+};
+
+const app = initializeApp(firebaseConfig);
